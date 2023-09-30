58638cff9b3026edcb398952efa6bddb7f971574
KeruiLiu <keruiliu@ufl.edu>
2023-09-30 19:35:14 -0400

    new starting menu script update: seamless background looping, control more than one background

--- Assets/Script/UI.cs
+++ Assets/Script/UI.cs
@@ -1,33 +1,41 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class UISceneController : MonoBehaviour
 {
-    public RectTransform background;     // The moving background
-    public float speed = 1.0f; // Speed at which the background moves
-    public bool loopBackground = true; // Should the background loop?
-    public float loopPoint; // At which point should the background loop?
+    public RectTransform[] backgrounds; // Array of the moving backgrounds
+    public float speed = 1.0f; // Speed at which the backgrounds move
+    public float backgroundWidth; // Width of the background images
 
-    private Vector2 originalPosition;
+    private Vector2[] originalPositions;
 
     void Start()
     {
-        originalPosition = background.anchoredPosition;
+        // Initialize the originalPositions array with the same length as backgrounds
+        originalPositions = new Vector2[backgrounds.Length];
+
+        // Store the original position of each background
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            originalPositions[i] = backgrounds[i].anchoredPosition;
+        }
     }
 
     void Update()
     {
-        MoveBackground();
+        // Move each background
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            MoveBackground(backgrounds[i], originalPositions[i]);
+        }
     }
 
-    void MoveBackground()
+    void MoveBackground(RectTransform background, Vector2 originalPosition)
     {
         Vector2 newPosition = background.anchoredPosition + new Vector2(-speed, 0) * Time.deltaTime;
         background.anchoredPosition = newPosition;
 
-        // If the background should loop and it's reached its loop point, reset its position
-        if (loopBackground && background.anchoredPosition.x <= loopPoint)
+        // If the background has moved off-screen, reset its position to the right to create a looping effect
+        if (background.anchoredPosition.x <= -backgroundWidth)
         {
             background.anchoredPosition = originalPosition;
         }
