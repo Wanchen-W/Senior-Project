b46e0fea02bea8163c2d306676a5d900e12bfb59
KeruiLiu <keruiliu@ufl.edu>
2023-12-06 20:40:26 -0500

    Mute

--- Assets/Script/Pause-Calling-action and logic.cs
+++ Assets/Script/Pause-Calling-action and logic.cs
@@ -134,26 +134,4 @@ public class SettingsMenuToggle : MonoBehaviour
 
     }
 
-    public class AudioManager : MonoBehaviour
-    {
-        public static AudioManager Instance; // Singleton instance
-
-        private void Awake()
-        {
-            if (Instance == null)
-            {
-                Instance = this;
-                DontDestroyOnLoad(gameObject);
-            }
-            else if (Instance != this)
-            {
-                Destroy(gameObject);
-            }
-        }
-
-        public void ToggleMute()
-        {
-            AudioListener.volume = AudioListener.volume == 0 ? 1 : 0;
-        }
-    }
 }
